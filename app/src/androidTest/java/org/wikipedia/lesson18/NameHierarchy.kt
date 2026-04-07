22ac01f4265cdf8334cc8ad824a511ee6608a81b
petukhov.vladislav <robertlokam@yandex.ru>
2026-04-07 21:12:19 +0300

    lesson18

---
+++ app/src/androidTest/java/org/wikipedia/lesson18/NameHierarchy.kt
@@ -0,0 +1,13 @@
+package org.wikipedia.lesson18
+
+class NameHierarchy(private val name: String, private val parent: NameHierarchy? = null) {
+
+    fun withParent(name: String): NameHierarchy {
+        return NameHierarchy(name, this)
+    }
+
+    override fun toString(): String {
+        return (parent?.let { "$it : " } ?: "") + name
+    }
+}
+
---
+++ app/src/androidTest/java/org/wikipedia/lesson18/Test.kt
@@ -0,0 +1,19 @@
+package org.wikipedia.lesson18
+
+import org.junit.Test
+
+//@Test
+//fun simpleTest() {
+//    run {
+//        OnboardingScreen {
+//            page(2) {
+//                step("Проверяет отображение элемента '${image.getName()}'") {
+//                    image.isDisplayed()
+//                }
+//            }
+//            step("Нажимает на '${skipButton.getName()}'") {
+//                skipButton.click()
+//            }
+//        }
+//    }
+//}
